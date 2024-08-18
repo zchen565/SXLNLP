04d28d98b512c6e80f4b9253ab6cc0044adf7906
Zihao Chen <37888282+zchen565@users.noreply.github.com>
2024-08-18 21:47:58 +0800

    Create loader.py

---
+++ chenzihao/week9/loader.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+
+import json
+import re
+import os
+import torch
+import random
+import jieba
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+
+from transformers import BertTokenizer
+
+import sys
+"""
+数据加载
+"""
+
+
+class DataGenerator:
+    def __init__(self, data_path, config):
+        self.config = config
+        self.path = data_path
+        # self.vocab = load_vocab(config["vocab_path"])
+        # self.config["vocab_size"] = len(self.vocab)
+        self.sentences = []
+        self.schema = self.load_schema(config["schema_path"])
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
+        self.load()
+
+    def load(self):
+        self.data = []
+        with open(self.path, encoding="utf8") as f:
+            segments = f.read().split("\n\n")
+            for segment in segments:
+                sentenece = []
+                labels = []
+                for line in segment.split("\n"):
+                    if line.strip() == "":
+                        continue
+                    char, label = line.split()
+                    sentenece.append(char)
+                    s = "".join(sentenece)
+                    labels.append(self.schema[label])
+
+
+
+                self.sentences.append("".join(sentenece))
+                # input_ids = self.encode_sentence(sentenece)
+                print("#"*80)
+                print(s)
+
+                input_ids = self.tokenizer(s, max_length = 102, padding="max_length", truncation=True, return_tensors="pt")
+
+                # print(len(input_ids))
+                input_ids = input_ids["input_ids"]
+                # print(type(input_ids))
+                # print(input_ids.shape)
+                input_ids = input_ids.squeeze()
+                # print(input_ids.shape)
+                
+                # print(len(labels))
+                labels = self.padding(labels, -1)
+                # print(len(labels))
+
+                # sys.exit()
+
+                # self.data.append([torch.LongTensor(input_ids), torch.LongTensor(labels)])
+                self.data.append([input_ids, torch.LongTensor(labels)])
+                '''
+                check whether this is the same form as our original model !!!
+                '''
+        return
+
+    # def encode_sentence(self, text, padding=True):
+    #     return self.tokenizer(text, padding=True, truncation=True, return_tensors="pt")
+        # input_id = []
+        # if self.config["vocab_path"] == "words.txt":
+        #     for word in jieba.cut(text):
+        #         input_id.append(self.vocab.get(word, self.vocab["[UNK]"]))
+        # else:
+        #     for char in text:
+        #         input_id.append(self.vocab.get(char, self.vocab["[UNK]"]))
+        # if padding:
+        #     input_id = self.padding(input_id)
+        # return input_id
+
+    #补齐或截断输入的序列，使其可以在一个batch内运算
+    def padding(self, input_id, pad_token=0):
+        input_id = input_id[:self.config["max_length"]]
+        input_id += [pad_token] * (self.config["max_length"] - len(input_id))
+        return input_id
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def load_schema(self, path):
+        with open(path, encoding="utf8") as f:
+            return json.load(f)
+
+#加载字表或词表
+# def load_vocab(vocab_path):
+#     token_dict = {}
+#     with open(vocab_path, encoding="utf8") as f:
+#         for index, line in enumerate(f):
+#             token = line.strip()
+#             token_dict[token] = index + 1  #0留给padding位置，所以从1开始
+#     return token_dict
+
+#用torch自带的DataLoader类封装数据
+def load_data(data_path, config, shuffle=True):
+    dg = DataGenerator(data_path, config)
+    dl = DataLoader(dg, batch_size=config["batch_size"], shuffle=shuffle)
+    return dl
+
+
+
+if __name__ == "__main__":
+    from config import Config
+    dg = DataGenerator("../ner_data/train.txt", Config)
+
